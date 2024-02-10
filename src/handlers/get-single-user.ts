5607bbef337591023da0c89d3fd8eb1ab2213d21
Ilya <rany4ka@gmail.com>
2024-02-10 13:14:17 +0300

    fix: GET method errors

--- src/handlers/get-single-user.ts
+++ src/handlers/get-single-user.ts
@@ -1,9 +1,10 @@
-import { users } from '../data'
 import { IncomingMessage, ServerResponse } from 'http'
+import { validate as validateUUID } from 'uuid'
+import { users } from '../data'
 
 export function getSingleUser(req: IncomingMessage, res: ServerResponse) {
   const userId = req.url?.split('/')[3]
-  if (!userId) {
+  if (!userId || !validateUUID(userId)) {
     res.statusCode = 400
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({ error: 'Invalid userId' }))
