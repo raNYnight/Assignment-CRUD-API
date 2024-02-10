5644efa9bcf2acd0ff2c8a1ce2aeb1d22845b770
Ilya <rany4ka@gmail.com>
2024-02-10 16:11:08 +0300

    feat: implement load balancer

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,14 @@
+import { IncomingMessage, ServerResponse } from 'http'
+
 export interface User {
   id: string
   username: string
   age: number
   hobbies: string[]
+}
+
+export interface RequestMessage {
+  type: 'request'
+  request: IncomingMessage
+  response: ServerResponse
 }
